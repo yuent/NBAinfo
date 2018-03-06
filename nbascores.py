8b5a431a53c7f4d39ab6f07e9624d88a68cac038
yuent <t.yuen17@gmail.com>
2018-03-05 19:56:59 -0500

    Delete nbascores.py

--- nbascores.py
+++
@@ -1,9 +0,0 @@
-import bs4, requests
-
-def getNBAscores():
-    res = requests.get('http://www.espn.com/nba/scoreboard')
-    soup = bs4.BeautifulSoup(res.text)
-    allScores = soup.find_all('div', class_='scoreboards')
-    print(allScores)
-
-getNBAscores()
